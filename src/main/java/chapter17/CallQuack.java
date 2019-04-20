a674c96d5a9c1f0d5b22b29e75a12a7f61be27fb
hoohack <hhqcontinue@gmail.com>
2019-04-20 09:34:43 +0800

    chapter17

--- src/main/java/chapter17/CallQuack.java
+++ src/main/java/chapter17/CallQuack.java
@@ -3,6 +3,9 @@ package chapter17;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+/**
+ * 使用反射调用quack方法
+ */
 public class CallQuack {
 
     public static void main(String[] args) {
